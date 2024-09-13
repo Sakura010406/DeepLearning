d6251ad81a69cbe0eb34a41ce46138d363d37e31
Sakura010406 <2378589062@qq.com>
2024-09-13 14:51:23 +0800

    delete all files

--- linreg.py
+++
@@ -1,9 +0,0 @@
-"""
-定义线性回归模型
-"""
-import torch
-
-
-def linreg(X, w, b):
-    """线性回归模型"""
-    return torch.matmul(X, w) + b
--- squared_loss.py
+++
@@ -1,8 +0,0 @@
-"""
-定义损失函数
-"""
-
-
-def squared_loss(y_true, y_pred):
-    """均方损失"""
-    return (y_true - y_pred.reshape(y_true.shape)) ** 2 / 2
